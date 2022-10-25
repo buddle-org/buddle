68ad2804e02b981fbc3e96770576bd1af0befbe8
Valentin B <valentin.be@protonmail.com>
2022-10-25 21:58:20 +0200

    op: Initialize crate

---
+++ src/buddle-object-property/src/lib.rs
@@ -0,0 +1,14 @@
+//! An implementation of the ObjectProperty reflection and
+//! serialization system.
+//!
+//! This is inspired by the original work of [Richard Lyle]
+//! as part of the [Medusa] project.
+//!
+//! [Richard Lyle]: https://github.com/rlyle
+//! [Medusa]: https://github.com/palestar/medusa
+
+#![deny(
+    rust_2018_idioms,
+    rustdoc::broken_intra_doc_links,
+    unsafe_op_in_unsafe_fn
+)]

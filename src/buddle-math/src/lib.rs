0b8129f9c892552990b3622cb2ddb6cf65ff7abc
Valentin B <valentin.be@protonmail.com>
2022-11-11 15:10:54 +0100

    math: Initialize crate

---
+++ src/buddle-math/src/lib.rs
@@ -0,0 +1,11 @@
+//! Linear algebra library for game maths.
+//!
+//! While this crate primarily just re-exports the types
+//! of the [`glam`] crate, a certain number of additional
+//! types which are used by the game are featured here.
+
+#![deny(rust_2018_idioms, rustdoc::broken_intra_doc_links)]
+#![forbid(unsafe_code)]
+
+#[doc(inline)]
+pub use glam::*;
